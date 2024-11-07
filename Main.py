59553f88946ccf10d5cbedd63d4e093807ddd525
Fabián Juárez <juarefa320@gmail.com>
2024-11-07 13:06:19 -0300

    ajusto codigo del main para evitar repeticion de codigo

--- Main.py
+++ Main.py
@@ -8,19 +8,19 @@ import Modelo_3_simplificado__seccion_3_6_CON_ROTACION
 from TraceFileGenerator import TraceFileGenerator
 
 if __name__ == '__main__':
-    EXECUTION_TIME=2
+    EXECUTION_TIME = 2
     generator = TraceFileGenerator("output.trc")
     
-    CASE_NAME, MODEL_NAME, modelStatus, solverStatus, objectiveValue, solverTime=Modelo_1_simplificado__seccion_2_8_overleaf_SIN_ROTACION.executeWithTimeLimit(EXECUTION_TIME)
-    generator.writeTraceRecord(CASE_NAME, MODEL_NAME, modelStatus, solverStatus, objectiveValue, solverTime)
-    CASE_NAME, MODEL_NAME, modelStatus, solverStatus, objectiveValue, solverTime=Modelo_1_simplificado__seccion_2_9_overleaf_CON_ROTACION.executeWithTimeLimit(EXECUTION_TIME)
-    generator.writeTraceRecord(CASE_NAME, MODEL_NAME, modelStatus, solverStatus, objectiveValue, solverTime)
-    CASE_NAME, MODEL_NAME, modelStatus, solverStatus, objectiveValue, solverTime=Modelo_2_simplificado__seccion_3_3_overleaf_SIN_ROTACION.executeWithTimeLimit(EXECUTION_TIME)
-    generator.writeTraceRecord(CASE_NAME, MODEL_NAME, modelStatus, solverStatus, objectiveValue, solverTime)
-    CASE_NAME, MODEL_NAME, modelStatus, solverStatus, objectiveValue, solverTime=Modelo_2_simplificado__seccion_3_4_overleaf_CON_ROTACION.executeWithTimeLimit(EXECUTION_TIME)
-    generator.writeTraceRecord(CASE_NAME, MODEL_NAME, modelStatus, solverStatus, objectiveValue, solverTime)
-    CASE_NAME, MODEL_NAME, modelStatus, solverStatus, objectiveValue, solverTime=Modelo_3_simplificado__seccion_3_5_SIN_ROTACION.executeWithTimeLimit(EXECUTION_TIME)
-    generator.writeTraceRecord(CASE_NAME, MODEL_NAME, modelStatus, solverStatus, objectiveValue, solverTime)
-    CASE_NAME, MODEL_NAME, modelStatus, solverStatus, objectiveValue, solverTime=Modelo_3_simplificado__seccion_3_6_CON_ROTACION.executeWithTimeLimit(EXECUTION_TIME)
-    generator.writeTraceRecord(CASE_NAME, MODEL_NAME, modelStatus, solverStatus, objectiveValue, solverTime)
+    models = [
+        Modelo_1_simplificado__seccion_2_8_overleaf_SIN_ROTACION,
+        Modelo_1_simplificado__seccion_2_9_overleaf_CON_ROTACION,
+        Modelo_2_simplificado__seccion_3_3_overleaf_SIN_ROTACION,
+        Modelo_2_simplificado__seccion_3_4_overleaf_CON_ROTACION,
+        Modelo_3_simplificado__seccion_3_5_SIN_ROTACION,
+        Modelo_3_simplificado__seccion_3_6_CON_ROTACION,
+    ]
+    
+    for model in models:
+        CASE_NAME, MODEL_NAME, modelStatus, solverStatus, objectiveValue, solverTime = model.executeWithTimeLimit(EXECUTION_TIME)
+        generator.writeTraceRecord(CASE_NAME, MODEL_NAME, modelStatus, solverStatus, objectiveValue, solverTime)
     
