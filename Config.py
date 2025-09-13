618e9df424f7d0147f85524cfbb779cc5e0d112b
jfa320 <juarefa320@gmail.com>
2025-09-13 16:39:29 -0300

    sigo subiendo casos de OR

--- Config.py
+++ Config.py
@@ -188,4 +188,41 @@ BIN_HEIGHT = 20         # alto del bin
 ITEM_WIDTH = 2         # ancho de cada ítem
 ITEM_HEIGHT = 8         # alto de cada ítem
 
+# Caso 3
+ITEMS_QUANTITY = 100     # ítems
+BIN_WIDTH = 20          # ancho del bin
+BIN_HEIGHT = 20         # alto del bin
+ITEM_WIDTH = 6         # ancho de cada ítem
+ITEM_HEIGHT = 5         # alto de cada ítem
+
+# Caso 4
+ITEMS_QUANTITY = 100     # ítems
+BIN_WIDTH = 20          # ancho del bin
+BIN_HEIGHT = 20         # alto del bin
+ITEM_WIDTH = 4         # ancho de cada ítem
+ITEM_HEIGHT = 9         # alto de cada ítem
+
+# Caso 5
+ITEMS_QUANTITY = 100     # ítems
+BIN_WIDTH = 20          # ancho del bin
+BIN_HEIGHT = 20         # alto del bin
+ITEM_WIDTH = 8         # ancho de cada ítem
+ITEM_HEIGHT = 7         # alto de cada ítem
+
+# Caso 6
+ITEMS_QUANTITY = 100     # ítems
+BIN_WIDTH = 20          # ancho del bin
+BIN_HEIGHT = 20         # alto del bin
+ITEM_WIDTH = 7         # ancho de cada ítem
+ITEM_HEIGHT = 5         # alto de cada ítem
+#optimo: 10
+
+# Caso 7
+ITEMS_QUANTITY = 100     # ítems
+BIN_WIDTH = 10          # ancho del bin
+BIN_HEIGHT = 30         # alto del bin
+ITEM_WIDTH = 1         # ancho de cada ítem
+ITEM_HEIGHT = 6         # alto de cada ítem
+# optimo 50
+
 ITEMS = list(range(1, ITEMS_QUANTITY + 1)) 
