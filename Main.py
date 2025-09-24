d36cbdb484ad571f6f08a3c4ffc5d1ec89d94778
Fabián Juárez <juarefa320@gmail.com>
2025-09-24 00:22:56 -0300

    organizo el repo moviendo modelos no usados a una carpeta

--- Main.py
+++ Main.py
@@ -1,31 +1,13 @@
 import Modelo_1_simplificado__seccion_2_8_overleaf_SIN_ROTACION 
 import Modelo_1_simplificado__seccion_2_9_overleaf_CON_ROTACION 
-import Modelo_2_simplificado__seccion_3_3_overleaf_SIN_ROTACION
-import Modelo_2_simplificado__seccion_3_4_overleaf_CON_ROTACION
-import Modelo_3_simplificado__seccion_3_5_SIN_ROTACION
-import Modelo_3_simplificado__seccion_3_6_CON_ROTACION
 import Modelo_5_Orquestador
 
 from TraceFileGenerator import TraceFileGenerator
 
 #Setear caso a probar en archivo Config.py
-
 if __name__ == '__main__':
     EXECUTION_TIME = 1200 # tiempo de ejecución en segundos para cada modelo (20 minutos)
     generator = TraceFileGenerator("output.trc")
-    
-    # models = [
-    #     Modelo_1_simplificado__seccion_2_8_overleaf_SIN_ROTACION,
-    #     Modelo_1_simplificado__seccion_2_9_overleaf_CON_ROTACION,
-    #     Modelo_2_simplificado__seccion_3_3_overleaf_SIN_ROTACION,
-    #     Modelo_2_simplificado__seccion_3_4_overleaf_CON_ROTACION,
-    #     Modelo_3_simplificado__seccion_3_5_SIN_ROTACION,
-    #     Modelo_3_simplificado__seccion_3_6_CON_ROTACION,
-    # ]
-    
-    # models = [
-    #     Modelo_5_Orquestador
-    # ]
     
     models = [
         Modelo_5_Orquestador,
